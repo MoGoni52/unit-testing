60d625a3a240bf976997f82173b9c780f2ac20f2
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-12 11:16:13 +0100

    fix each tests

--- getLargest.test.js
+++ getLargest.test.js
@@ -1,3 +1,5 @@
+const each = require('jest-each').default;
+
 const getLargest = require("./getLargest");
 
 describe("getLargest", () => {
@@ -5,6 +7,7 @@ describe("getLargest", () => {
     test("getLargest is a function", () => {
         expect(typeof getLargest).toBe("function");
     })
+})
 
     describe ("It handles normal inputs successfully", () => {
         
@@ -13,6 +16,6 @@ describe("getLargest", () => {
             [[9, 8, 7], 9],
             [[12, 36, 19], 36]
         ]).test("", (arr, expected) => {
-            expect(getLargest(arr).toEqual(expected));
+            expect(getLargest(...arr)).toEqual(expected);
         })  
-        })
+    })
