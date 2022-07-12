612ff7d6face15fd2e7c5c2980fd127a90a9fd52
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-12 11:50:15 +0100

    change numberfunction and update git-ignore

--- numberFunctions.js
+++ numberFunctions.js
@@ -4,5 +4,9 @@ const getLargest = (a, b, c) => {
         return Math.max(a, b, c);
     }
 
+const getSmallest = (a, b ,c) => {
+        return Math.min(a, b ,c);
+}
+
 // Export the function so it's available to other files
 module.exports = getLargest;
