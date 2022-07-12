487653245554bbcba4633ea1f08c0bd250b8c76d
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-12 20:37:23 +0100

    added pluralizedString function js file

---
+++ patterns.js
@@ -0,0 +1,5 @@
+const patterns = () => {
+    return `& & & & &\n& & & & &\n& & & & &\n& & & & &\n& & & & &\n& & & & &`;
+}
+
+console.log(patterns());
---
+++ pluralizedString.js
@@ -0,0 +1,4 @@
+let item = "Car";
+let count = 1;
+
+count > 1 ? console.log(`I have ${count} ${item}s`) : console.log(`I have ${count} ${item}`);
